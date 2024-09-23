724b377e998e1bff8e7f73b3efd7b78997edc525
wjriemer <wjriemer@crimson.ua.edu>
2024-09-22 20:15:27 -0500

    Trained model on roboflow dataset & ran it on video from youtube.

--- CV/cv_service.py
+++ CV/cv_service.py
@@ -1,1 +1,17 @@
 # This file will contain main logic for cv operations
+
+from ultralytics import YOLO
+
+def main():
+    model = YOLO('models/best.pt')
+
+    results = model.predict('input_videos/bball_2.mp4', save=True)
+
+    print(results[0])
+    print('==========================')
+    for box in results[0].boxes:
+        print(box)
+
+
+if __name__ == '__main__':
+    main()
